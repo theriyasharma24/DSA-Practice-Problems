4656e4ce161a1f7441b03bb051b03b434fa912cf
Riya Sharma <theriyasharma24@gmail.com>
2023-10-18 12:24:47 +0530

    Update 4 Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.py

--- 1 Array/4 Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.py
+++ 1 Array/4 Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.py
@@ -2,7 +2,21 @@
 
 class Solution:
     def sort012(self,arr,n):
-        arr.sort()
+        left, curr, right = 0, 0, len(arr) - 1
+
+        while curr <= right:
+            if arr[curr] == 0:
+                # Swap the element at the left pointer with the current element
+                arr[left], arr[curr] = arr[curr], arr[left]
+                left += 1
+                curr += 1
+            elif arr[curr] == 1:
+                curr += 1
+            else:  # nums[curr] == 2
+                # Swap the element at the right pointer with the current element
+                arr[curr], arr[right] = arr[right], arr[curr]
+                right -= 1
+        return arr
         # code here
 
 

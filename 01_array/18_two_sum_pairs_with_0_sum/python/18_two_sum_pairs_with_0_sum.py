1b6b970582147f1ed68fbe1eb5ce617f2c817aed
Riya Sharma <theriyasharma24@gmail.com>
2025-01-08 22:30:23 +0530

    added problem no. 18 solution in python (#41)

---
+++ 01_array/18_two_sum_pairs_with_0_sum/python/18_two_sum_pairs_with_0_sum.py
@@ -0,0 +1,65 @@
+class Solution:
+    def getPairs(self, arr):
+        seen = set()         
+        result = set()  
+        for num in arr:
+            if -num in seen: 
+                result.add(tuple(sorted([num, -num])))  
+            seen.add(num) 
+        return [list(pair) for pair in sorted(result)]
+                    
+            
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  # array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+class IntMatrix:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n, m):
+        matrix = []
+        # matrix input
+        for _ in range(n):
+            matrix.append([int(i) for i in input().strip().split()])
+        return matrix
+
+    def Print(self, arr):
+        for i in arr:
+            for j in i:
+                print(j, end=" ")
+            print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+
+        obj = Solution()
+        res = obj.getPairs(arr)
+        if len(res) == 0:
+            print()
+        else:
+            IntMatrix().Print(res)
+        print("~")
+# } Driver Code Ends
+
+#Time Complexity - O(nlogn)
+#Space Complexity - O(n)        
+

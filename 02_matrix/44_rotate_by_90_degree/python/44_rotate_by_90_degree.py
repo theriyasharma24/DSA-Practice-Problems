d79dfedebc4a7f256fefed4712b4b89048b16533
Riya Sharma <theriyasharma24@gmail.com>
2025-02-19 18:59:28 +0530

    added problem no. 44 solution in python (#101)

---
+++ 02_matrix/44_rotate_by_90_degree/python/44_rotate_by_90_degree.py
@@ -0,0 +1,34 @@
+def rotate(mat): 
+    n = len(mat)
+    m = len(mat[0])
+    for i in range(n - 1):
+        for j in range(i, m):
+            mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+    for i in range(n):
+        mat[i].reverse()
+    return mat
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        matrix = []
+        for i in range(N):
+            arr = [int(x) for x in input().strip().split()]
+            matrix.append(arr)
+
+        rotate(matrix)
+        for i in range(N):
+            for j in range(N):
+                print(matrix[i][j], end=' ')
+            print()
+        print("~")
+
+# } Driver Code Ends
+#Time Complexity - O(n^2)
+#Space Complexity - O(1)
